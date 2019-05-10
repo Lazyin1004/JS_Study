f143fb87d7fc091fffbce38142b41406653a11a1
djffk124 <49220536+djffk124@users.noreply.github.com>
2019-05-10 11:55:42 +0900

    new

---
+++ ZeroCho(clone_cording)/for반복문 연습.js
@@ -0,0 +1,40 @@
+
+//  for(var star = 1 ; star <= 5 ; star +=1){
+//      console.log('*'.repeat(star))
+//  }
+
+
+// for(var star = 5 ; star >= 1 ; star -= 1){
+//     console.log('*'.repeat(star))
+// }
+
+
+// for(var star = 10 ; star >= 2 ; star -=2){
+//     console.log('*'.repeat(star))
+// }
+
+// for(var star = 1 ; star <= 16 ; star *=2){
+//     console.log('*'.repeat(star))
+// }
+
+// for(var star = 5 ; star >=1; star -=1){
+//     console.log(' '.repeat(5 - star) + '*'.repeat(star))
+// }
+
+// for(var star = 9; star >=1; star -=2){
+//     console.log(' '.repeat(9-star/2)+'*'.repeat(star)+'')
+// }
+
+// for(var star = 0; star <5; star +=1){
+//     console.log('*'.repeat(star+1));
+// 
+
+var 원진 = {
+    이름: '최원진',
+    키: 172,
+    몸무게: 78,
+    먹다: function eat(){
+        console.log("냠냠냠냠")
+    }
+}
+console.log('안녕하세요'[3])
